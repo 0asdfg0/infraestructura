5faabc28049fa576c12f65d21a7ceb3df307f0dc
0asdfg0 <niconam2009@hotmail.com>
2020-11-11 14:57:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ HoverCarousel.prototype = {
         this.prevMore = this.DOM.wrap.scrollLeft > 0
         this.nextMore = this.scrollWidth - this.containerWidth - this.DOM.wrap.scrollLeft > 5
 
-        this.DOM.scope.setAttribute('data-at',
+        ts.DOM.scope.setAttribute('data-at',
             (this.prevMore  ? 'left ' : ' ')
             + (this.nextMore ? 'right' : '')
         )
